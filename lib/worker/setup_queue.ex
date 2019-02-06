5af56a9b29410b9eb08e8f80c44bb120f44fcac4
sescobb27 <s.escob.b.27@gmail.com>
2019-02-06 15:02:10 -0500

    [add] support for binding options [add] random queue generation for tests [add] testing queue bindings

--- lib/worker/setup_queue.ex
+++ lib/worker/setup_queue.ex
@@ -7,19 +7,21 @@ defmodule ExRabbitPool.Worker.SetupQueue do
 
   def init({pool_id, rabbitmq_config}) do
     adapter = rabbitmq_config |> Keyword.get(:adapter, ExRabbitPool.RabbitMQ)
-    queues = rabbitmq_config |> Keyword.fetch!(:queues)
+    queues = rabbitmq_config |> Keyword.get(:queues, [])
 
     for queue_config <- queues do
       queue_name = queue_config |> Keyword.fetch!(:queue_name)
       exchange = queue_config |> Keyword.fetch!(:exchange)
       queue_options = queue_config |> Keyword.get(:queue_options, [])
       exchange_options = queue_config |> Keyword.get(:exchange_options, [])
+      bind_options = queue_config |> Keyword.get(:bind_options, [])
 
       # Fail if couldn't create queue
       :ok =
         ExRabbitPool.create_queue_with_bind(adapter, pool_id, queue_name, exchange, :direct,
           queue_options: queue_options,
-          exchange_options: exchange_options
+          exchange_options: exchange_options,
+          bind_options: bind_options
         )
     end
 
--- test/integration/setup_queue_test.exs
+++ test/integration/setup_queue_test.exs
@@ -1,9 +1,19 @@
 defmodule ExRabbitPool.Integration.SetupQueueTest do
   use ExUnit.Case, async: false
 
+  alias ExRabbitPool.Worker.SetupQueue
+
   @moduletag :integration
-  @queue1 "test.queue1"
-  @queue2 "test.queue2"
+
+  defp random_queue_name() do
+    rnd =
+      8
+      |> :crypto.strong_rand_bytes()
+      |> Base.url_encode64()
+      |> binary_part(0, 8)
+
+    "test.queue-" <> rnd
+  end
 
   setup do
     caller = self()
@@ -11,20 +21,6 @@ defmodule ExRabbitPool.Integration.SetupQueueTest do
     rabbitmq_config = [
       channels: 1,
       port: String.to_integer(System.get_env("EX_RABBIT_POOL_PORT") || "5672"),
-      queues: [
-        [
-          queue_name: @queue1,
-          exchange: "my_exchange",
-          queue_options: [auto_delete: true],
-          exchange_options: [auto_delete: true]
-        ],
-        [
-          queue_name: @queue2,
-          exchange: "my_exchange2",
-          queue_options: [auto_delete: true],
-          exchange_options: [auto_delete: true]
-        ]
-      ],
       caller: caller
     ]
 
@@ -48,18 +44,90 @@ defmodule ExRabbitPool.Integration.SetupQueueTest do
       type: :supervisor
     })
 
-    {:ok, pool_id: :setup_queue_pool}
+    {:ok, pool_id: :setup_queue_pool, queue1: random_queue_name(), queue2: random_queue_name()}
   end
 
-  test "declare queue on startup", %{pool_id: pool_id} do
+  test "declare queue on startup", %{pool_id: pool_id, queue1: queue1, queue2: queue2} do
+    start_supervised!(
+      {SetupQueue,
+       {pool_id,
+        [
+          queues: [
+            [
+              queue_name: queue1,
+              exchange: "my_exchange",
+              queue_options: [auto_delete: true],
+              exchange_options: [auto_delete: true]
+            ],
+            [
+              queue_name: queue2,
+              exchange: "my_exchange2",
+              queue_options: [auto_delete: true],
+              exchange_options: [auto_delete: true]
+            ]
+          ]
+        ]}}
+    )
+
     ExRabbitPool.with_channel(pool_id, fn {:ok, channel} ->
       assert :ok = AMQP.Basic.publish(channel, "my_exchange", "", "Hello, World!")
-      assert {:ok, "Hello, World!", _meta} = AMQP.Basic.get(channel, @queue1)
-      assert {:ok, _} = AMQP.Queue.delete(channel, @queue1)
+      assert {:ok, "Hello, World!", _meta} = AMQP.Basic.get(channel, queue1, no_ack: true)
 
       assert :ok = AMQP.Basic.publish(channel, "my_exchange2", "", "Hell Yeah!")
-      assert {:ok, "Hell Yeah!", _meta} = AMQP.Basic.get(channel, @queue2)
-      assert {:ok, _} = AMQP.Queue.delete(channel, @queue2)
+      assert {:ok, "Hell Yeah!", _meta} = AMQP.Basic.get(channel, queue2, no_ack: true)
+
+      assert {:ok, _} = AMQP.Queue.delete(channel, queue1)
+      assert {:ok, _} = AMQP.Queue.delete(channel, queue2)
+    end)
+  end
+
+  test "declare queues with multiple bindings on startup", %{
+    pool_id: pool_id,
+    queue1: queue1,
+    queue2: queue2
+  } do
+    start_supervised!(
+      {SetupQueue,
+       {pool_id,
+        [
+          queues: [
+            [
+              queue_name: queue1,
+              exchange: "X",
+              queue_options: [auto_delete: true],
+              exchange_options: [auto_delete: true],
+              bind_options: [routing_key: "orange"]
+            ],
+            [
+              queue_name: queue2,
+              exchange: "X",
+              queue_options: [auto_delete: true],
+              exchange_options: [auto_delete: true],
+              bind_options: [routing_key: "black"]
+            ],
+            [
+              queue_name: queue2,
+              exchange: "X",
+              queue_options: [auto_delete: true],
+              exchange_options: [auto_delete: true],
+              bind_options: [routing_key: "green"]
+            ]
+          ]
+        ]}}
+    )
+
+    ExRabbitPool.with_channel(pool_id, fn {:ok, channel} ->
+      assert :ok = AMQP.Basic.publish(channel, "X", "orange", "Hello, World!")
+      assert {:ok, "Hello, World!", _meta} = AMQP.Basic.get(channel, queue1, no_ack: true)
+
+      assert :ok = AMQP.Basic.publish(channel, "X", "black", "Hola Mundo!")
+      assert {:ok, "Hola Mundo!", _meta} = AMQP.Basic.get(channel, queue2, no_ack: true)
+
+      assert :ok = AMQP.Basic.publish(channel, "X", "green", "Olá Mundo!")
+      assert {:ok, "Olá Mundo!", _meta} = AMQP.Basic.get(channel, queue2, no_ack: true)
+
+      assert {:ok, _} = AMQP.Queue.delete(channel, queue1)
+      assert {:ok, _} = AMQP.Queue.delete(channel, queue2)
     end)
   end
 end
