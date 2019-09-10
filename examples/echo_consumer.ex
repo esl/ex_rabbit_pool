c97a0f680f50a4ceb44b130e37e0820ab786a71f
Humberto Figuera <hfiguera@gmail.com>
2019-09-10 17:49:46 -0500

    [fix] Use the correct function clauses to be able to overwrite the default ones.

--- examples/echo_consumer.ex
+++ examples/echo_consumer.ex
@@ -8,7 +8,7 @@ defmodule Example.EchoConsumer do
   ################################
 
   # Confirmation sent by the broker after registering this process as a consumer
-  def basic_consume_ok(_adapter, _channel, _consumer_tag) do
+  def basic_consume_ok(_state, _consumer_tag) do
     Logger.info("[consumer] successfully registered as a consumer (basic_consume_ok)")
     :ok
   end
@@ -16,20 +16,20 @@ defmodule Example.EchoConsumer do
   # This is sent for each message consumed, where `payload` contains the message
   # content and `meta` contains all the metadata set when sending with
   # Basic.publish or additional info set by the broker;
-  def basic_deliver(adapter, channel, payload, %{delivery_tag: delivery_tag}) do
+  def basic_deliver(%{adapter: adapter, channel: channel}, payload, %{delivery_tag: delivery_tag}) do
     Logger.info("[consumer] consuming payload (#{inspect payload})")
     :ok = adapter.ack(channel, delivery_tag, requeue: false)
     :ok
   end
 
   # Sent by the broker when the consumer is unexpectedly cancelled (such as after a queue deletion)
-  def basic_cancel(_adapter, _channel, _consumer_tag, _no_wait) do
+  def basic_cancel(_state, _consumer_tag, _no_wait) do
     Logger.error("[consumer] consumer was cancelled by the broker (basic_cancel)")
     :ok
   end
 
   # Confirmation sent by the broker to the consumer process after a Basic.cancel
-  def basic_cancel_ok(_adapter, _channel, _consumer_tag) do
+  def basic_cancel_ok(_state, _consumer_tag) do
     Logger.error("[consumer] consumer was cancelled by the broker (basic_cancel_ok)")
     :ok
   end
